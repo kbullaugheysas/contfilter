068db6401f73287107a9b456d7cb93598af095fe
Kevin Bullaughey <bullaugh@sas.upenn.edu>
2019-05-20 17:10:08 -0400

    seems to be working; now writing to a bam file

---
+++ bam.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+)
+
+type BamScanner struct {
+	LineNumber int
+	filename   string
+	stdin      bool
+	scanner    *bufio.Scanner
+	wg         sync.WaitGroup
+	prev       string
+	record     []string
+	Closed     bool
+}
+
+func (s *BamScanner) OpenBam(bamfile string) error {
+	s.filename = bamfile
+	cmd := exec.Command("samtools", "view", bamfile)
+	input, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("failed creating pipe: %v", err)
+	}
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("command failed to start: %v", err)
+	}
+	s.scanner = bufio.NewScanner(input)
+	s.wg.Add(1)
+	go func() {
+		s.wg.Wait()
+
+		if !s.stdin {
+			if err := cmd.Wait(); err != nil {
+				log.Fatal("wait failed: ", err)
+			}
+		}
+	}()
+	return nil
+}
+
+// Fast forward to the next record with read name `read`
+func (s *BamScanner) Find(read string) ([]string, error) {
+	for {
+		if s.Closed {
+			// Reached the end of the file.
+			return nil, nil
+		}
+		record, err := s.Record()
+		if err != nil {
+			return nil, err
+		}
+		if record[0] == read {
+			s.Ratchet()
+			return record, nil
+		}
+		if strnum_cmp(record[0], read) < 0 {
+			// Not far enough yet
+			s.Ratchet()
+		} else {
+			// We didn't find the read before we reached one that is past what
+			// we're looking for. We'll leave this one in the cache in case we
+			// search for it next.
+			return nil, nil
+		}
+	}
+}
+
+func (s *BamScanner) Record() ([]string, error) {
+	if s.record != nil {
+		return s.record, nil
+	}
+	s.Closed = !s.scanner.Scan()
+	if err := s.scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner of %s errored: %v", s.filename, err)
+	}
+	if s.Closed {
+		return nil, nil
+	}
+	line := strings.TrimSpace(s.scanner.Text())
+	s.LineNumber++
+	if len(line) == 0 {
+		return nil, fmt.Errorf("empty BAM record")
+	}
+	s.record = strings.Split(line, "\t")
+	if len(s.record) == 0 {
+		return nil, fmt.Errorf("empty record at line %s", s.LineNumber)
+	}
+	read := s.record[0]
+	if s.prev != "" {
+		if strnum_cmp(s.prev, read) > 0 {
+			return nil, fmt.Errorf("sorting order violated at line %d", s.LineNumber)
+		}
+	}
+	s.prev = read
+	return s.record, nil
+}
+
+func (s *BamScanner) Ratchet() {
+	s.record = nil
+}
+
+func (s *BamScanner) Done() {
+	s.wg.Done()
+}
+
+func (s *BamScanner) OpenStdin() {
+	s.filename = "stdin"
+	s.stdin = true
+	s.wg.Add(1)
+	s.scanner = bufio.NewScanner(os.Stdin)
+}
+
+func ReadBamHeader(bamfile string) (string, error) {
+	output, err := exec.Command("samtools", "view", "-H", bamfile).Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to read header: %v", err)
+	}
+	return string(output), nil
+}
+
+type BamWriter struct {
+	filename string
+	wg       sync.WaitGroup
+	fp       *os.File
+}
+
+func (w *BamWriter) Open(bamfile string) (io.WriteCloser, error) {
+	w.filename = bamfile
+	cmd := exec.Command("samtools", "view", "-b", "-o", bamfile, "-")
+	fp, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, fmt.Errorf("failed creating pipe: %v", err)
+	}
+	w.wg.Add(1)
+	go func() {
+		samOut, err := cmd.CombinedOutput()
+		if len(samOut) > 0 {
+			log.Println("samtools output:")
+			log.Print(string(samOut))
+		}
+		if err != nil {
+			log.Fatal("executing samtools for writing bam file failed: ", err)
+		}
+		w.wg.Done()
+	}()
+	return fp, nil
+}
+
+func (w *BamWriter) Wait() {
+	w.wg.Wait()
+}
--- contfilter.go
+++ contfilter.go
@@ -1,160 +1,270 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
-	"sync"
-	"unicode"
+	"time"
 )
 
-func digitToInt(s string) int {
-	n, err := strconv.Atoi(s)
-	if err != nil {
-		log.Fatal("failed to parse digit ", err)
-	}
-	return n
-}
-
-// From: https://github.com/samtools/samtools/blob/develop/bam_sort.c#L13
-func strnum_cmp(as, bs string) int {
-	a := []rune(as)
-	b := []rune(bs)
-	i := 0
-	j := 0
-	for i < len(a) && j < len(b) {
-		if unicode.IsDigit(a[i]) && unicode.IsDigit(b[j]) {
-			for i < len(a) && a[i] == '0' {
-				i++
-			}
-			for j < len(b) && b[j] == '0' {
-				j++
-			}
-			for i < len(a) && j < len(b) && unicode.IsDigit(a[i]) && unicode.IsDigit(b[j]) && a[i] == b[j] {
-				i++
-				j++
-			}
-			// By this point we've forwarded across any leading zeros && any digits that match.
-			// Next we get determine if they have the same number of digits
-			// before the first non-diget. If so we use the numerical values of
-			// the number formed by these digits to determine order.
-			if i < len(a) && j < len(b) && unicode.IsDigit(a[i]) && unicode.IsDigit(b[j]) {
-				k := 0
-				for i+k < len(a) && unicode.IsDigit(a[i+k]) && j+k < len(b) && unicode.IsDigit(b[j+k]) {
-					k += 1
-				}
-				if i+k < len(a) && unicode.IsDigit(a[i+k]) {
-					return 1
-				} else if j+k < len(b) && unicode.IsDigit(b[j+k]) {
-					return -1
-				} else {
-					return digitToInt(string(a[i:(i+k)])) - digitToInt(string(b[j:(j+k)]))
-				}
-			} else if i < len(a) && unicode.IsDigit(a[i]) {
-				return 1
-			} else if j < len(b) && unicode.IsDigit(b[j]) {
-				return -1
-			} else if i != j {
-				if i < j {
-					return 1
-				}
-				return -1
-			}
-		} else {
-			if a[i] != b[j] {
-				return digitToInt(string(a[i])) - digitToInt(string(b[j]))
-			}
-			i++
-			j++
-		}
-	}
-	if len(a) > len(b) {
-		return 1
-	} else if len(a) < len(b) {
-		return -1
-	}
-	return 0
-}
-
 var sample string
+var margin float64
+var minsamplen int
+var mincontlen int
+var maxdist int
+var limit int
+var penalty float64
+var output string
+var filterErcc bool
+var logfn string
 
 func init() {
 	log.SetFlags(0)
-	flag.StringVar(&sample, "sample", "", "BAM file of the sample you want to filter (sorted by name)")
+	flag.StringVar(&sample, "sample", "", "BAM file of the sample you want to filter (sorted by name, required)")
+	flag.Float64Var(&margin, "margin", 1.0, "how much better sample needs to be matched")
+	flag.IntVar(&minsamplen, "min-samp-len", 60, "min length for a sample match")
+	flag.IntVar(&mincontlen, "min-cont-len", 70, "min length for a contamination match")
+	flag.IntVar(&maxdist, "max-edit-dist", 5, "max edit distance for a sample match")
+	flag.IntVar(&limit, "limit", 0, "limit the number of sample reads considered (0 = no limit)")
+	flag.Float64Var(&penalty, "edit-penalty", 2.0, "multiple for how to penalize edit distance")
+	flag.StringVar(&output, "output", "", "output bam file (required)")
+	flag.StringVar(&logfn, "log", "", "keep a record of what happens to each read")
+	flag.BoolVar(&filterErcc, "ercc", false, "exclude ERCC mappings from sample before filtering")
 	flag.Usage = func() {
-		log.Println("usage: contfilter -sample sample.bam cont1.bam cont2.bam")
-		log.Println("   or: samtools view sample.bam | contfilter cont1.bam cont2.bam")
+		log.Println("usage: contfilter [options] cont1.bam cont2.bam")
 		flag.PrintDefaults()
 	}
 }
 
+func benchmark(start time.Time, label string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", label, elapsed)
+}
+
+func extract(row []string) (int, int, error) {
+	if len(row) < 15 {
+		return 0, 0, fmt.Errorf("too few fields")
+	}
+	match_len := len(row[9])
+	edit_tag := row[14]
+	if edit_tag[:5] != "nM:i:" {
+		return 0, 0, fmt.Errorf("malformed edit distance tag: %s", edit_tag)
+	}
+	edit_dist, err := strconv.Atoi(edit_tag[5:])
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to parse edit dist: %s", edit_tag)
+	}
+	return match_len, edit_dist, nil
+}
+
 func main() {
+	var kept_percent float64
 	flag.Parse()
 	contamination := flag.Args()
+	startedAt := time.Now()
 
 	if len(contamination) == 0 {
 		log.Println("Must specify at least one contamination mapping BAM file")
 		os.Exit(1)
 	}
 
-	var scanner *bufio.Scanner
-	var cmd *exec.Cmd
-	if sample == "" {
-		scanner = bufio.NewScanner(os.Stdin)
-	} else {
-		// Check that the file exists by trying to open it. We don't read anything from it.
-		f, err := os.Open(sample)
-		if err != nil {
-			log.Fatal("failed to open ", sample)
-		}
-		f.Close()
-
-		cmd = exec.Command("samtools", "view", sample)
-		bam, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal("failed creating pipe: ", err)
-		}
-		if err := cmd.Start(); err != nil {
-			log.Fatal("command failed to start: ", err)
-		}
-		scanner = bufio.NewScanner(bam)
+	if output == "" {
+		log.Println("Must specify -output file")
+		os.Exit(1)
 	}
-	var wg sync.WaitGroup
-	var prev string
-	var line_no int
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
 
-		if sample != "" {
-			if err := cmd.Wait(); err != nil {
-				log.Fatal("wait failed: ", err)
+	scanner := BamScanner{}
+	if sample == "" {
+		scanner.OpenStdin()
+	} else {
+		if err := scanner.OpenBam(sample); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	reads_found := make([]int, len(contamination))
+	reads_filtered := make([]int, len(contamination))
+	contScanners := make([]BamScanner, len(contamination))
+	rejected := make([]bool, len(contamination))
+	found := make([]bool, len(contamination))
+
+	for c := 0; c < len(contamination); c++ {
+		if err := contScanners[c].OpenBam(contamination[c]); err != nil {
+			log.Fatal(err)
+		}
+		reads_found[c] = 0
+		reads_filtered[c] = 0
+	}
+
+	header, err := ReadBamHeader(sample)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := BamWriter{}
+	outfp, err := out.Open(output)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.WriteString(outfp, header)
+
+	var logfile *os.File
+	if logfn != "" {
+		log.Println("Opening log file", logfn)
+		logfile, err = os.Create(logfn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer logfile.Close()
+		if logfile != nil {
+			log.Println("Opened log file", logfn)
+		}
+	}
+
+	reads_kept := 0
+	total_reads := 0
+	ercc := 0
+	considered := 0
+	too_short := 0
+	too_diverged := 0
+
+	err = func() error {
+		defer scanner.Done()
+		defer benchmark(startedAt, "processing")
+
+		for {
+			for c, _ := range contamination {
+				rejected[c] = false
+				found[c] = false
+			}
+			record, err := scanner.Record()
+			if err != nil {
+				return fmt.Errorf("failed to read from sample BAM: %v after %d lines", err, total_reads)
+			}
+			if scanner.Closed {
+				return nil
+			}
+			scanner.Ratchet()
+
+			if total_reads > 0 && total_reads%100000 == 0 {
+				kept_percent = float64(reads_kept) / float64(considered) * 100
+				log.Printf("considered %d out of %d so far, kept %0.1f%%\n", considered, total_reads, kept_percent)
+			}
+			if limit > 0 && limit == total_reads {
+				return nil
+			}
+			total_reads++
+			sample_len, sample_edit_dist, err := extract(record)
+			if err != nil {
+				return err
+			}
+			if sample_len < minsamplen {
+				too_short++
+				continue
+			}
+			if sample_edit_dist > maxdist {
+				too_diverged++
+				continue
+			}
+			sample_score := float64(sample_len) - float64(sample_edit_dist)*penalty
+			read := record[0]
+			if filterErcc && strings.Contains(record[2], "ERCC") {
+				ercc++
+				continue
+			}
+			considered++
+			// Reads in the sample BAM will be rejected if either mate in any of the
+			// contamination BAM files maps better than in the sampel BAM file.
+			was_rejected := false
+			for c := 0; c < len(contamination); c++ {
+				for m := 0; m < 2; m++ {
+					mate, err := contScanners[c].Find(read)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if mate != nil {
+						if !found[c] {
+							found[c] = true
+							reads_found[c]++
+						}
+						length, edit_dist, err := extract(mate)
+						if err != nil {
+							log.Fatalf("failed to read from %s: %v", contamination[c], err)
+						}
+						if length >= mincontlen {
+							score := float64(length) - float64(edit_dist)*penalty
+							if sample_score <= score+margin {
+								if !rejected[c] {
+									reads_filtered[c]++
+									rejected[c] = true
+									was_rejected = true
+									if logfile != nil {
+										entry := fmt.Sprintf("read %s with length %d and edit distance %d was rejected with score %0.1f because in %s it had a score of %0.1f with length %d and edit distance %d\n", read, sample_len, sample_edit_dist, sample_score, contamination[c], score, length, edit_dist)
+										logfile.WriteString(entry)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			if !was_rejected {
+				// This read is okay, output it to the output BAM file.
+				_, err := fmt.Fprintf(outfp, "%s\n", strings.Join(record, "\t"))
+				if err != nil {
+					return err
+				}
+				reads_kept++
+				if logfile != nil {
+					entry := fmt.Sprintf("Kept read %s with length %d and edit distance %d and score %0.1f\n",
+						read, sample_len, sample_edit_dist, sample_score)
+					logfile.WriteString(entry)
+				}
 			}
 		}
 	}()
-
-	for scanner.Scan() {
-		line_no += 1
-		line := scanner.Text()
-		line = strings.TrimSpace(line)
-		fields := strings.Split(line, "\t")
-		read_name := fields[0]
-		if prev != "" {
-			if strnum_cmp(prev, read_name) > 0 {
-				log.Fatalf("sorting order violated at line %d", line_no)
-			}
-		}
-		prev = read_name
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal("scanner errored: ", err)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	wg.Wait()
-	fmt.Println("OK", line_no)
+	outfp.Close()
+	out.Wait()
+
+	if filterErcc {
+		erccPerc := float64(ercc) / float64(total_reads) * 100
+		log.Printf("filtered out %d ERCC reads (%0.1f%%) before comparing to contamination\n", ercc, erccPerc)
+	}
+
+	shortPerc := float64(too_short) / float64(total_reads) * 100
+	log.Printf("filtered out %d reads (%0.1f%%) becase their alignment was too short\n", too_short, shortPerc)
+	divergedPerc := float64(too_diverged) / float64(total_reads) * 100
+	log.Printf("filtered out %d reads (%0.1f%%) becase they were too diverged\n", too_diverged, divergedPerc)
+
+	for c, cont := range contamination {
+		n := reads_filtered[c]
+		perc := float64(n) / float64(considered) * 100
+		found_perc := float64(reads_found[c]) / float64(considered) * 100
+		log.Printf("found %d of %d reads in %s (%0.1f%%)\n", reads_found[c], considered, cont, found_perc)
+		log.Printf("rejected %d of %d reads from %s (%0.1f%%)\n", reads_filtered[c], considered, cont, perc)
+	}
+
+	kept_percent = float64(reads_kept) / float64(considered) * 100
+	total_percent := float64(reads_kept) / float64(total_reads) * 100
+	log.Printf("kept %d of %d reads (%0.1f%%), which is %0.1f%% of the total\n",
+		reads_kept, considered, kept_percent, total_percent)
+
+	log.Println("machine parsable stats:")
+	stats := []int{total_reads, ercc, considered, reads_kept}
+	stats = append(stats, reads_found...)
+	stats = append(stats, reads_filtered...)
+	fmt.Print("stats")
+	for _, s := range stats {
+		fmt.Printf("\t%d", s)
+	}
+	fmt.Print("\n")
 }
